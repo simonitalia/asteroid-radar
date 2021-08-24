b4436ac0f2938b8d3fa2e20cf7c710fb8750339e
Simon Italia <sitalia@magicaltomato.com>
2021-08-24 18:28:53 +1000

    - add view model live data observer logic
    - docs: add code comments

--- app/src/main/java/com/udacity/asteroidradar/ui/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/main/MainFragment.kt
@@ -1,14 +1,17 @@
 package com.udacity.asteroidradar.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.models.Asteroid
 
 /**
- * This fragment shows the the status of the Nasa NEO (Near-Earth-Objects) web services transaction.
+ * This fragment shows asteroids from fetched from Nasa NEO (Near-Earth-Objects) web service.
  */
 
 class MainFragment: Fragment() {
@@ -28,6 +31,27 @@ class MainFragment: Fragment() {
 
         setHasOptionsMenu(true) //show options menu in action bar
         return binding.root
+    }
+
+    /**
+     * Called immediately after onCreateView() has returned, and fragment's
+     * view hierarchy has been created.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //monitor changes to MainViewModel live data
+        viewModel.asteroids.observe(viewLifecycleOwner, Observer<List<Asteroid>> { asteroids ->
+            asteroids?.apply {
+
+                Log.i("MainFragment", "Asteroids successfully loaded from repo: ${asteroids.count()}")
+
+//                viewModelAdapter?.asteroids = asteroids
+            }
+        })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
