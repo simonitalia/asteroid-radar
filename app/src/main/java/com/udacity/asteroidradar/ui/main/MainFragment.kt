759c60603da38dbaded92396a059ff87361e80a7
Simon Italia <sitalia@magicaltomato.com>
2021-08-24 17:35:22 +1000

    update viewModel reference with MainViewModel.Factory

--- app/src/main/java/com/udacity/asteroidradar/ui/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/main/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.udacity.asteroidradar.main
+package com.udacity.asteroidradar.ui.main
 
 import android.os.Bundle
 import android.view.*
@@ -13,9 +13,9 @@ import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment: Fragment() {
 
-    // lazily initialize viewModel
+    // lazily initialize MainViewModel using .Factory to pass in application parameter
     private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
+        ViewModelProvider(this, MainViewModel.Factory(activity!!.application)).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
