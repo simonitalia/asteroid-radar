96f891452680b108ff2d4ee81db2cdbc75144dbc
Simon Italia <sitalia@magicaltomato.com>
2021-08-25 12:17:47 +1000

    add new database related values

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY = "4hv6w6nW0oxcsDB6XRSe3JxVCJ4P3l9LMphd46Ll"
+    const val NEO_TABLE = "near_earth_objects_table"
+    const val DATABASE_VERSION = 1
 }
