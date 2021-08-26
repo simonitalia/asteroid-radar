cf9c50e5aa026e51015c824af706b1c88b5eaeb8
Simon Italia <sitalia@magicaltomato.com>
2021-08-26 12:26:35 +1000

    add picture of day database table name string

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -6,5 +6,6 @@ object Constants {
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY = "4hv6w6nW0oxcsDB6XRSe3JxVCJ4P3l9LMphd46Ll"
     const val NEO_TABLE = "near_earth_objects_table"
+    const val POD_TABLE = "picture_of_the_day_table"
     const val DATABASE_VERSION = 1
 }
