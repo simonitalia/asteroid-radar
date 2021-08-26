be2c566c1d207a485757d7053cf2ac8caf7f7957
Simon Italia <sitalia@magicaltomato.com>
2021-08-26 18:38:23 +1000

    delete unnecessary constants

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -2,10 +2,6 @@ package com.udacity.asteroidradar
 
 object Constants {
 
-    enum class MediaType(value: String) {
-        VIDEO("video"), IMAGE("image")
-    }
-
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
