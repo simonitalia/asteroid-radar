7d9629bb86eb72ab702cb8e924a010aff444ea33
Simon Italia <sitalia@magicaltomato.com>
2021-08-26 12:47:52 +1000

    delete unneeded libraries

--- app/src/main/java/com/udacity/asteroidradar/database/Room.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.udacity.asteroidradar.Constants
-import com.udacity.asteroidradar.models.Asteroid
 
 
 /**
